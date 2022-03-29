08734a32cdeac76ee270755bf788249728baaa7d
jungmin00 <101387361+jungmin00@users.noreply.github.com>
2022-03-29 14:40:18 +0900

    Update hello.py
    7~13줄 함수 추가했습니다. 검토 부탁드립니다.

--- hello.py
+++ hello.py
@@ -2,3 +2,12 @@ Heewon Kim, 202001196, Divison of Global Sport Industry
 
 a = "Hello Python"
 print(a)
+
+
+print(안녕하세요)
+def.포켓몬빵드셔보셨나요?
+
+   if 답 is Yes:
+    Print(와 부럽다 전 파이리빵이 먹어보고 싶어요)
+   else 답 is No:
+    print(역시 포켓몬빵은 구하기 힘든가봐요)
